9bc6f2eb1bc60fac736b37b87dd69a4b1bcae6ad
henriquelbk <loubackhenrique123@gmail.com>
2023-02-15 21:31:24 -0300

    att

---
+++ exercicio1/script.js
@@ -0,0 +1,20 @@
+const sacolao = []
+
+const banana = {
+    nome: `banana`,
+    preco: 1.5,
+    disponibilidade: true
+}
+const morango = {
+    nome: `morango`,
+    preco: 10,
+    disponibilidade: true
+}
+const uva = {
+    nome: `uva`,
+    preco: 5,
+    disponibilidade: true
+}
+
+sacolao.push(banana, morango, uva)
+console.log(sacolao)
